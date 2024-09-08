ba09798c9ab7a2f292c0ca3ae2129fa81949ae2f
94peterchoi <94peterchoi@gmail.com>
2024-09-08 18:45:23 +0900

    Add: routes for diary and signup featurs

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,11 @@ import MainView from '@/views/MainView.vue'
 import WordView from '@/views/WordView.vue'
 import LoginView from '@/views/LoginView.vue'
 import Layout from '@/components/Layout.vue'
-import WordList from '@/components/WordList.vue'
-
+import WordListView from '@/views/WordListView.vue'
+import WriteDiaryView from '@/views/WriteDiaryView.vue'
+import DiaryListView from '@/views/DiaryListView.vue'
+import DiaryDetailView from '@/views/DiaryDetailView.vue'
+import SignupView from '@/views/SignupView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +16,11 @@ const router = createRouter({
       path: '/login',
       name: 'Login',
       component: LoginView
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: SignupView,
     },
     {
       path: '/',
@@ -31,7 +39,22 @@ const router = createRouter({
         {
           path: 'word/list/mine',
           name: 'WordList',
-          component: WordList
+          component: WordListView
+        },
+        {
+          path: 'diary/write',
+          name: 'WrtingDiaryPage',
+          component: WriteDiaryView
+        },
+        {
+          path: 'diary/official-category/list',
+          name: 'DiaryList',
+          component: DiaryListView
+        },
+        {
+          path: 'diary/:id/detail',
+          name: 'DiaryDetail',
+          component: DiaryDetailView
         }
       ]
     }
