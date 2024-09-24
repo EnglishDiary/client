e96fab0fe7e35987219b80911b672bd2f22b92cc
94peterchoi <94peterchoi@gmail.com>
2024-09-24 16:29:50 +0900

    feat: create component for 404 error handling

---
+++ src/components/NotFound.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="fullscreen bg-grey-1 text-center q-pa-md flex flex-center">
+        <div>
+            <div style="font-size: 30vh">
+                404
+            </div>
+
+            <div class="text-h2" style="opacity:.4">
+                Oops! 페이지를 찾을 수 없습니다.
+            </div>
+
+            <q-btn class="q-mt-xl" color="primary" text-color="black" unelevated to="/" label="홈으로 돌아가기" no-caps />
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.q-page {
+    min-height: 100vh;
+}
+</style>
