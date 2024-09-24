3f67c337dc7119becbaa1d8683bec50227de19e9
94peterchoi <94peterchoi@gmail.com>
2024-09-24 16:32:16 +0900

    feat: enhance WriteExpression component
    - add dialog for saving expressions
    - disable save button if none of items is checked

--- src/components/WriteExpression.vue
+++ src/components/WriteExpression.vue
@@ -13,16 +13,17 @@
                 <q-btn label="AI에게 물어보기" color="primary" @click="askAI" :loading="loading" />
             </q-card-actions>
         </q-card>
-
         <!-- AI Response -->
         <q-card v-if="aiResponse" class="q-mt-md q-pa-md">
             <q-card-section>
-                <h2 class="text-h6">AI의 답변</h2>
+                <!-- <h2 class="text-h6">AI의 답변</h2> -->
                 <p>{{ aiResponse.summary }}</p>
-
-                <h3 class="text-subtitle1 q-mt-md">영작 결과:</h3>
+                <!-- <h3 class="text-subtitle1 q-mt-md">영작 결과:</h3> -->
                 <q-list bordered padding>
                     <q-item v-for="(result, index) in aiResponse.translations" :key="index">
+                        <q-item-section avatar>
+                            <q-checkbox v-model="result.checked" @update:model-value="updateCheckedState(index)" />
+                        </q-item-section>
                         <q-item-section>
                             <q-item-label>{{ result.english }}</q-item-label>
                             <q-item-label caption>{{ result.korean }}</q-item-label>
@@ -31,15 +32,40 @@
                 </q-list>
             </q-card-section>
             <q-card-actions align="right">
-                <q-btn label="저장하기" color="primary" @click="askAI" :loading="loading" />
+                <q-btn label="저장하기" color="primary" @click="openPublishModal" :disable="!canSave" />
             </q-card-actions>
-
         </q-card>
+
+        <!-- 발행 모달 -->
+        <q-dialog v-model="showPublishModal">
+            <q-card style="min-width: 350px">
+                <q-card-section>
+                    <div class="text-h6">표현집에 저장하기</div>
+                </q-card-section>
+
+                <q-card-section class="q-pt-none">
+                    <q-radio v-model="publishSettings.isPublic" :val=true label="공개" />
+                    <!-- <q-radio v-model="publishSettings.isPublic" :val=false label="비공개" /> -->
+
+                    <!-- <q-select v-model="selectedCategory" :options="categoryOptions" label="카테고리"
+                        @update:model-value="handleCategoryChange" option-value="id" option-label="name"
+                        class="q-mt-md" /> -->
+
+                </q-card-section>
+
+                <q-card-actions align="right">
+                    <q-btn flat label="취소" color="primary" v-close-popup />
+                    <q-btn flat label="저장" color="primary" @click="saveExpression" v-close-popup />
+                </q-card-actions>
+            </q-card>
+        </q-dialog>
+
+
     </q-page>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useQuasar } from 'quasar'
 import { apiCall } from '@/utils/apiCall'
 import { API_LIST } from '@/utils/apiList'
@@ -49,6 +75,12 @@ const koreanInput = ref('')
 const englishInput = ref('')
 const aiResponse = ref(null)
 const loading = ref(false)
+const canSave = ref(true)
+const showPublishModal = ref(false)
+const publishSettings = ref({
+    isPublic: true,
+    categoryId: null,
+})
 
 const askAI = async () => {
     if (!koreanInput.value) {
@@ -67,8 +99,58 @@ const askAI = async () => {
     loading.value = false
     if (response.status) {
         aiResponse.value = response.data
+        aiResponse.value.translations.forEach(translation => {
+            translation.checked = true
+        })
+    }
+}
+
+const updateCheckedState = (index) => {
+    console.log(`Translation ${index + 1} checked state updated:`, aiResponse.value.translations[index].checked)
+
+    let checkedItemCount = 0
+    aiResponse.value.translations.forEach((item) => {
+        if (item.checked) {
+            checkedItemCount++
+        }
+    })
+    if (checkedItemCount == 0) {
+        canSave.value = false
+        return
+    }
+    canSave.value = true
+}
+
+const openPublishModal = () => {
+    showPublishModal.value = true
+}
+
+const saveExpression = async () => {
+    const checkedTranslations = aiResponse.value.translations.filter(t => t.checked)
+    const parameters = {
+        originalSentence: aiResponse.value.originalSentence,
+        userSentence: englishInput.value,
+        summary: aiResponse.value.summary,
+        translations: checkedTranslations,
     }
 
-
+    const response = await apiCall(API_LIST.SAVE_EXPRESSION, parameters)
+    if (response.status) {
+        $q.notify({
+            color: 'positive',
+            message: response.message
+        })
+    }
 }
+
+// Watch for changes in aiResponse
+watch(() => aiResponse.value, (newValue) => {
+    if (newValue) {
+        newValue.translations.forEach(translation => {
+            if (translation.checked === undefined) {
+                translation.checked = true
+            }
+        })
+    }
+}, { deep: true })
 </script>
