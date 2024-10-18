86f1501d555cfde9a9e4752ad9fd9c422fab2599
94peterchoi <94peterchoi@gmail.com>
2024-10-18 15:16:40 +0900

    feat: Add utility method for array shuffle

---
+++ src/utils/commonUtils.js
@@ -0,0 +1,11 @@
+const commonUtils = {
+    shuffleArray: (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+}
+
+export { commonUtils }
