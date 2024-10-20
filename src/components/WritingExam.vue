6d6d11845dd0131543d2e8afe01fb179b09d2e08
94peterchoi <94peterchoi@gmail.com>
2024-10-21 03:20:19 +0900

    feat: Add features
    - disable buttons when user finish displacement
    - enhance creating random words displacement
    - change way to compare user sentence with correct sentence

--- src/components/WritingExam.vue
+++ src/components/WritingExam.vue
@@ -4,8 +4,13 @@ import { API_LIST } from '@/utils/apiList'
 import { apiCall } from '@/utils/apiCall'
 import { commonUtils } from '@/utils/commonUtils';
 
+const props = defineProps({
+    practiceInfo: Object,
+})
+
 const currentData = ref(null)
 const userResult = ref('')
+const finished = ref(false)
 
 const totalResult = []
 let originalData = []
@@ -15,12 +20,19 @@ let countOfClickedWords = 0
 
 
 const fetchExamSentences = async () => {
-    const response = await apiCall(API_LIST.FETCH_EXAM_SENTENCES)
+    const response = await apiCall(API_LIST.FETCH_EXAM_SENTENCES, props.practiceInfo)
     if (response.status) {
         originalData = commonUtils.shuffleArray(response.data)
 
         originalData.forEach((item) => {
-            const words = item.sourceSentence.split(' ')
+            let words
+            words = item.sourceSentence.split(' ')
+            if (words.length >= 16) {
+                words = commonUtils.combineWords(words, 3)
+            } else if (words.length >= 8) {
+                words = commonUtils.combineWords(words, 2)
+            }
+
             item.randomWords = commonUtils.shuffleArray(words)
             item.totalWordCount = words.length
             item.wordOrders = []
@@ -28,27 +40,6 @@ const fetchExamSentences = async () => {
         })
 
         currentData.value = originalData[currentIndex]
-    }
-}
-
-const toggleWord = (selectedWord, index) => {
-    const clickedArray = currentData.value.isWordClicked
-
-    if (!clickedArray[index]) {
-        countOfClickedWords++
-        clickedArray[index] = true
-        currentData.value.wordOrders.push(index)
-    } else {
-        countOfClickedWords--
-        clickedArray[index] = false
-        currentData.value.wordOrders = currentData.value.wordOrders.filter((order) => {
-            return order !== index
-        })
-    }
-
-    if (currentData.value.totalWordCount == countOfClickedWords) {
-        checkUserSentence()
-        moveToNextWord()
     }
 }
 
@@ -65,6 +56,7 @@ const toggleWordSelection = (selectedWord, index) => {
     }
 
     if (totalWordCount == countOfClickedWords) {
+        finished.value = true
         checkUserSentence()
         moveToNextWord()
     }
@@ -73,18 +65,13 @@ const toggleWordSelection = (selectedWord, index) => {
 
 const checkUserSentence = () => {
     const correctSentence = currentData.value.sourceSentence
-    const correctWordOrders = correctSentence.split(' ')
-
-    let isCorrect = true
     let userSentence = ''
-    currentData.value.wordOrders.forEach((order, index) => {
+    currentData.value.wordOrders.forEach((order) => {
         userSentence += currentData.value.randomWords[order] + ' '
-        if (correctWordOrders[index] != currentData.value.randomWords[order]) {
-            isCorrect = false
-        }
     })
+    userSentence = userSentence.trim()
 
-    if (isCorrect) {
+    if (userSentence == correctSentence) {
         userResult.value = 'Great :)'
         currentData.value.isCorrect = true
     } else {
@@ -105,7 +92,8 @@ const moveToNextWord = () => {
         userResult.value = ''
 
         currentData.value = originalData[currentIndex]
-    }, 3000);
+        finished.value = false
+    }, 4000);
 }
 
 const reset = () => {
@@ -129,11 +117,11 @@ onMounted(() => {
 
         <div class="word-groups" v-for="(word, index) in currentData.randomWords" :key="word.id">
             <q-btn @click="toggleWordSelection(word, index)"
-                :color="currentData.isWordClicked[index] ? 'secondary' : 'info'">
+                :color="currentData.isWordClicked[index] ? 'secondary' : 'info'" :disable="finished">
                 {{ word }}</q-btn>
         </div>
 
-        <q-btn color="primary" @click="reset">Reset</q-btn>
+        <q-btn color="primary" @click="reset" :disable="finished">Reset</q-btn>
         <br>
 
         <div>
