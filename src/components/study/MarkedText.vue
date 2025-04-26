cbb886f9baa67c100a2a55231d9e20ef9f98e48c
94peterchoi <94peterchoi@gmail.com>
2025-04-26 22:54:06 +0900

    feat: create component for rendering markdown

---
+++ src/components/study/MarkedText.vue
@@ -0,0 +1,24 @@
+<template>
+    <div v-html="renderedMarkdown" class="markdown-body"></div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { marked } from 'marked'
+
+const props = defineProps({
+    content: {
+        type: String,
+        required: true
+    }
+})
+
+const renderedMarkdown = computed(() => marked(props.content))
+</script>
+
+<style>
+/* 깃허브 스타일 등 마크다운 CSS 필요 시 추가 */
+.markdown-body {
+    line-height: 1.6;
+}
+</style>
